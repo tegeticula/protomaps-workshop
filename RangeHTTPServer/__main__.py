cada60c541c1603e6b744bfe341290f951f877c1
John Keefe <john@reallygoodsmarts.nyc>
2025-03-21 11:14:00 -0400

    initial setup

---
+++ RangeHTTPServer/__main__.py
@@ -0,0 +1,31 @@
+"""
+Use this in the same way as Python's SimpleHTTPServer:
+
+  python -m RangeHTTPServer [port]
+
+The only difference from SimpleHTTPServer is that RangeHTTPServer supports
+'Range:' headers to load portions of files. This is helpful for doing local web
+development with genomic data files, which tend to be to large to load into the
+browser all at once.
+"""
+
+try:
+    # Python3
+    import http.server as SimpleHTTPServer
+
+except ImportError:
+    # Python 2
+    import SimpleHTTPServer
+
+from . import RangeRequestHandler
+
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('port', action='store',
+                    default=8000, type=int,
+                    nargs='?', help='Specify alternate port [default: 8000]')
+parser.add_argument('-b', '--bind', metavar='ADDRESS',
+                    help='bind to this address (default: all interfaces)')
+
+args = parser.parse_args()
+SimpleHTTPServer.test(HandlerClass=RangeRequestHandler, port=args.port, bind=args.bind)
